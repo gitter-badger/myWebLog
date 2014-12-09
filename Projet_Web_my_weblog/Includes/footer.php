5ea5e34ae48d13c0b10592c9a6b745d257a3fe53
mesagf <mesagouiffes@gmail.com>
2014-12-09 15:38:58 +0100

    ajout du head

--- Projet_Web_my_weblog/Includes/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-	<h2>Made with <span>❤</span> by Christophe Coquelet, Sofiane Béors, Florian Mesa--Gouiffes - Samsung Campus France.</h2>
-</footer>
--- Projet_Web_my_weblog/Includes/head.php
+++
@@ -1,3 +0,0 @@
-	<meta name="description" content="Description de ma page web." />
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="../CSS/style.css" />
