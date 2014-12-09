f097f84dba842bc0f9e1d27eec64e6996a58e41e
mesagf <mesagouiffes@gmail.com>
2014-12-09 15:52:59 +0100

    repush

--- Projet_Web_my_weblog/admin.php
+++ Projet_Web_my_weblog/admin.php
@@ -9,18 +9,18 @@
 <body>
 	<?php include("includes/menu.php"); ?>
 	
-<div id="compte">
-	<p>Nombre de blog :</p>
-	<p>Nombre de post :</p>
-	<p>Nombre de likes :</p>
-</div>
-<div id="gestion">
-	<ul>
-		<li><a href="#">Gestion des membres</a></li>
-		<li><a href="#">Forum</a></li>
-		<li><a href="#">Livre d'or</a></li>
-	</ul>
-</div>
+	<div id="compte">
+		<p>Nombre de blog :</p>
+		<p>Nombre de post :</p>
+		<p>Nombre de likes :</p>
+	</div>
+	<div id="gestion">
+		<ul>
+			<li><a href="#">Gestion des membres</a></li>
+			<li><a href="#">Forum</a></li>
+			<li><a href="#">Livre d'or</a></li>
+		</ul>
+	</div>
 	<?php include("Includes/footer.php"); ?>
 </body>
 </html>
