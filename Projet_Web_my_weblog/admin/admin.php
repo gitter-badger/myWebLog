95c2ad9dcfbb7bbeff9689d5e96d8ccd5aba08d1
mesagf <mesagouiffes@gmail.com>
2014-12-09 15:46:58 +0100

    Page admin

---
+++ Projet_Web_my_weblog/admin/admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>admin</title>
+	<meta name="description" content="Description de ma page web." />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="../CSS/style.css" />
+</head>
+<body>
+	<?php include("includes/menu.php"); ?>
+	<p>Nombre de blog :</p>
+	<p>Nombre de post :</p>
+	<p>Nombre de likes :</p>
+
+	<ul>
+		<li><a href="#">Gestion des membres</a></li>
+		<li><a href="#">Forum</a></li>
+		<li><a href="#">Livre d'or</a></li>
+	</ul>
+
+	<?php include("Includes/footer.php"); ?>
+</body>
+</html>
