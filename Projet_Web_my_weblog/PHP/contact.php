0468350d7fdc233773ce978dad5ba980337f38de
mesagf <mesagouiffes@gmail.com>
2014-12-10 16:59:33 +0100

    contact

---
+++ Projet_Web_my_weblog/PHP/contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>admin</title>
+	<meta name="description" content="Description de ma page web." />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="../CSS/style.css" />
+</head>
+<body>
+
+	<div id="contactMail">
+			<h2 id="h2Contact">Contactez nous</h2>
+		<form method="POST" action="" id="contact">
+
+			<input type="text" name="nom" id="nom" class="clmail" placeholder="Nom"/>
+			<input type="text" name="object" id="object" class="clmail" placeholder="objet"/>
+			<input type="text" name="messageMail" id="messageMail" class="clmail" placeholder="Le message a nous faire passer ..."/>
+			<input type="submit" id="validMail"/>	
+
+
+		</form>
+	</div>
+</div>
+<?php include('../Includes/footer.php'); ?>
+</body>
+</html>
