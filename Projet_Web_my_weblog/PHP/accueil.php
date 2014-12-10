faa273d149eeb4ebe099a4b5f1b0ef54901bbf24
mesagf <mesagouiffes@gmail.com>
2014-12-10 16:45:21 +0100

    Html Contact

--- Projet_Web_my_weblog/PHP/accueil.php
+++ Projet_Web_my_weblog/PHP/accueil.php
@@ -2,8 +2,8 @@
 	include('accueilSQL.php');
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<!DOCTYPE html>
+<html>
 <head>
 	<title>Accueil</title>
 	<meta name="description" content="Description de ma page web." />
