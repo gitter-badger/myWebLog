a9b1f1c16d36767e226b4a6cfc37f7253405d859
beors_s <beors_s@epitech.eu>
2014-12-11 17:27:06 +0100

    mail

---
+++ Projet_Web_my_weblog/PHP/contactSQL.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+include("myPDO.php");
+
+	$message = $_POST['messageMail'];
+
+	$obj = "De " . $_SESSION['name'] . " : " . $_POST['object'] . ", " . $_SESSION['mail'];
+
+	mail("sofiane.beors@epitech.eu", $obj, $message);
+
+?>
+
+	<script type="text/javascript">alert("Votre mail a bien ete envoye");</script>
+
+<?php
+
+	header("Refresh:1; accueil.php");
+
+?>
