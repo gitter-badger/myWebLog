ff5b99510d844daedff5097cdfadf99937f1ad1c
florian.mesa-gouiffes <florian.mesa-gouiffes@epitech.eu>
2014-12-11 15:44:07 +0100

    billet

--- Projet_Web_my_weblog/PHP/contact.php
+++ Projet_Web_my_weblog/PHP/contact.php
@@ -12,7 +12,7 @@
 			<h2 id="h2Contact">Contactez nous</h2>
 		<form method="POST" action="" id="contact">
 
-			<input type="text" name="nom" id="nom" class="clmail" placeholder="Nom"/>
+			<input type="text" name="nom" id="nomMail" class="clmail" placeholder="Nom"/>
 			<input type="text" name="object" id="object" class="clmail" placeholder="objet"/>
 			<input type="text" name="messageMail" id="messageMail" class="clmail" placeholder="Le message a nous faire passer ..."/>
 			<input type="submit" id="validMail"/>	
